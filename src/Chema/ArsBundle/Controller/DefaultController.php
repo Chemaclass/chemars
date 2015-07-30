15023cd5925ac00d70a188cabf54c024bae6ce00
Chemaclass <chemaclass@outlook.es>
2015-07-30 19:34:55 +0200

    Add decorator pattern: layout base file in Resources/views

--- src/Chema/ArsBundle/Controller/DefaultController.php
+++ src/Chema/ArsBundle/Controller/DefaultController.php
@@ -9,11 +9,21 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+    	return [];
+    }
+
+    /**
+     * @Route("/load-json")
+     * @Template()
+     */
+    public function loadJsonAction()
+    {
+
+    	return $this->render('ChemaArsBundle:Main:loadJson.html.twig');
     }
 }
